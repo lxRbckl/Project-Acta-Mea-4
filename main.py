0dfcb1f272d32c27c381ca75496a2b8e156d72fd
ala2q6 <lxrbckl@protonmail.com>
2022-08-04 02:46:44 -0500

    program push

---
+++ main.py
@@ -0,0 +1,162 @@
+# Project Acta Mea by Alex Arbuckl #
+
+
+# import <
+from os import path
+from github import Github
+from discord import Intents
+from discord.ext import commands
+from lxRbckl import githubGet, githubSet, jsonLoad
+
+# >
+
+
+# global <
+gFile = ''
+gRepository = ''
+gPath = path.realpath(__file__).split('/')
+gDirectory = '/'.join(gPath[:(len(gPath) - 1)])
+githubToken = ''
+actaMea = commands.Bot(command_prefix = '', intents = Intents.all())
+discordToken = ''
+
+# >
+
+
+async def setFunction(ctx, pKeyA: str, pKeyB: str, pElement: str, pData: dict):
+    '''  '''
+
+    # if (new node) <
+    # elif (then existing node) <
+    if ((pKeyA not in pData.keys()) and (not pKeyB) and (not pElement)): var = pData[pKeyA] = {}
+    elif (pKeyA in pData.keys()):
+
+        # if (value for node) <
+        # elif (service for node services) <
+        if (pKeyB): var = pData[pKeyA][pKeyB] = pElement if (pElement) else []
+        elif ((pKeyB == 'service') and (pElement)): pData[pKeyA][pKeyB].append(pElement); var = 1
+
+        # >
+
+    # >
+
+    # if (data change) <
+    if (var): githubSet(
+
+        pData = pData,
+        pFile = gFile,
+        pRepository = gRepository,
+        pGithub = Github(githubToken)
+
+    )
+
+    # >
+
+
+async def getFunction(ctx, pKeyA: str, pKeyB: str, pElement: str, pData: dict):
+    '''  '''
+
+    # if (all nodes) <
+    # if (node values) <
+    # if (values of node value) <
+    if ((not pKeyA) and (not pKeyB)): var = pData.keys()
+    elif (pKeyB in pData[pKeyA].keys()): var = pData[pKeyA][pKeyB].keys()
+    elif ((not pKeyB) and (pKeyA in pData.keys())): var = pData[pKeyA].keys()
+
+    # >
+
+    # if (data) <
+    if (var): await ctx.channel.send(
+
+        delete_after = 60,
+        content = '\n'.join(f'`{i}`' for i in var)
+
+    )
+
+    # >
+
+
+async def whereFunction(ctx, pKeyA: str, pKeyB: str, pElement: str, pData: dict):
+    '''  '''
+
+    # get data <
+    # filter data <
+    var = {k : [i.lower().split('-') for i in v['running']] for k, v in pData.items()}
+    var = [k for k in var.items() for i in v if (pKeyA in i)]
+
+    # >
+
+    # if (data) <
+    if (len(var) > 0): await ctx.channel.send(
+
+        delete_after = 60,
+        content = '\n'.join(f'`{i}`' for i in var)
+
+    )
+
+    # >
+
+
+async def deleteFunction(ctx, pKeyA: str, pKeyB: str, pElement: str, pData: dict):
+    '''  '''
+
+    # if (node) <
+    # else (then existing node) <
+    if ((pKeyA in pData.keys()) and (not pKeyB) and (not pElement)): del pData[pKeyA]; var = 1
+    elif ((pKeyA in pData.keys()) and (pKeyB or pElement)):
+
+        # if (value for node) <
+        # elif (service for node services) <
+        if (pKeyB): del pData[pKeyA][pKeyB]; var = 1
+        elif ((pKeyB == 'service') and (pElement)): pData[pKeyA][pKeyB].remove(pElement); var = 1
+
+        # >
+
+    # >
+
+    # if (data change) <
+    if (var): githubSet(
+
+        pData = pData,
+        pFile = gFile,
+        pRepository = gRepository,
+        pGithub = Github(githubToken)
+
+    )
+
+    # >
+
+
+@commands.has_permissions(administrator = True)
+@actaMea.command(aliases = jsonLoad(pFile = f'{gDirectory}/setting.json')['aliases'])
+async def commandFunction(ctx, pKey: str = None, pValue: str = None, pElement: str = None):
+    '''  '''
+
+    await {
+
+        'set' : setFunction,
+        'get' : getFunction,
+        'where' : whereFunction,
+        'delete' : deleteFunction
+
+    }[ctx.invoked_with.lower()](
+
+        ctx = ctx,
+        pKeyA = pKey,
+        pKeyB = pValue,
+        pElement = pElement,
+        pData = githubGet(
+
+            pFile = gFile,
+            pRepository = gRepository,
+            pGithub = Github(githubToken)
+
+        )
+
+    )
+
+
+# main <
+if (__name__ == '__main__'): actaMea.run(discordToken)
+
+# >
